5e59d3ff65bc30f45cce00feb22ccbdc6f365278
Luis <112443531+LuisS1991@users.noreply.github.com>
2025-12-29 22:00:52 -0300

    Remove frontend_port from rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -2,10 +2,10 @@ import reflex as rx
 
 config = rx.Config(
     app_name="web",
-    frontend_port=80,
     db_url="sqlite:///examen_2025.db",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ]
+
 )
