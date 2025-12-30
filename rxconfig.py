f507860ff2248f38f32850b9d08b7a094b0a3629
Luis <112443531+LuisS1991@users.noreply.github.com>
2025-12-29 22:22:20 -0300

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,11 +1,15 @@
 import reflex as rx
-
 config = rx.Config(
     app_name="web",
     db_url="sqlite:///examen_2025.db",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://web-2514y8nbl8ye.up-de-fra1-k8s-1.apps.run-on-seenode.com"
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ]
 
 )
+
