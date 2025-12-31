f067ac60afb902f739d3a45577887d3b8f72c9b5
LuisS1991 <nantesluis72@gmail.com>
2025-12-31 08:04:56 -0300

    update rxconf

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,5 @@
 import os
 import reflex as rx
-<<<<<<< HEAD
 from dotenv import load_dotenv
 
 # Cargar .env
@@ -15,19 +14,4 @@ config = rx.Config(
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ],
-=======
-config = rx.Config(
-    app_name="web",
-    db_url="sqlite:///examen_2025.db",
-    cors_allowed_origins=[
-        "http://localhost:3000",
-        "https://web-2514y8nbl8ye.up-de-fra1-k8s-1.apps.run-on-seenode.com"
-    ],
-    plugins=[
-        rx.plugins.SitemapPlugin(),
-        rx.plugins.TailwindV4Plugin(),
-    ]
-
->>>>>>> 7ad2bc4bad4e5b921d71de5b56014990930fe39d
 )
-
